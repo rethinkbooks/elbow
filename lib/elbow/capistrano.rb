f2ed49367b8199ad207a0cb19cd7f1dc05de0a1b
Arturo K <arturo@bookshout.com>
2015-12-11 22:32:03 +0000

    Use load balancer name.

--- lib/elbow/capistrano.rb
+++ lib/elbow/capistrano.rb
@@ -6,18 +6,13 @@ def elastic_load_balancer(name, *args)
 
     include Capistrano::DSL
 
-    packet = Net::DNS::Resolver.start(name)
-    all_cnames= packet.answer.reject { |p| !p.instance_of? Net::DNS::RR::CNAME }
-    cname = all_cnames.find { |c| c.name == "#{name}."}
-    cname = cname.cname[0..-2]
-
     aws_region= fetch(:aws_region, 'us-east-1')
     AWS.config(:access_key_id => fetch(:aws_access_key_id),
         :secret_access_key => fetch(:aws_secret_access_key),
         :ec2_endpoint => "ec2.#{aws_region}.amazonaws.com",
         :elb_endpoint => "elasticloadbalancing.#{aws_region}.amazonaws.com")
 
-    load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == cname.downcase }
+    load_balancer = AWS::ELB.new.load_balancers[name]
     raise "EC2 Load Balancer not found for #{name} in region #{aws_region}" if load_balancer.nil?
 
     load_balancer.instances.each do |instance|
